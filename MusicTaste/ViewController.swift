0623a9266e058f5b507070207ffeca3df596b812
Fernando <fernanddolucas@gmail.com>
2021-02-03 09:07:27 -0300

    Initial Commit

---
+++ MusicTaste/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MusicTaste
+//
+//  Created by Fernando de Lucas da Silva Gomes on 03/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
